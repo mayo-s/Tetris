1b76b421b694b5189084d60c2b8157d84f776729
Mario Schuetz <mayo2@gmx.de>
2018-04-04 19:21:10 +0200

    Remove old code

--- src/beans/Tetromino.java
+++ src/beans/Tetromino.java
@@ -2,25 +2,6 @@ package beans;
 
 public class Tetromino {
 	
-//	public enum Tetromino {
-//		O("o"), I("i"), J("j"), L("l"), S("s"), Z("z"), T("t");
-//
-//		private String type;
-//
-//		private Tetromino(String type) {
-//			this.type = type;
-//		}
-//
-//		public static Tetromino getTetromino(String type) {
-//			for (Tetromino tetromino : values()) {
-//				if (type.equalsIgnoreCase(tetromino.type)) {
-//					return tetromino;
-//				}
-//			}
-//			return null;
-//		}
-//	}
-	
 	private Block[] blocks;
 	
 	public Tetromino(Block b1, Block b2, Block b3, Block b4) {
