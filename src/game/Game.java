d6701937bf3b9112d2f8838fdcbb4848c3ba004b
Mario Schuetz <mayo2@gmx.de>
2018-04-12 10:39:44 +0200

    Remove Stack, Add Queue

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,28 +2,32 @@ package game;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Game {
 
 	private List<Tetromino> tetrominos;
 	private final int LIMIT = 2;
-	private Stack<Tetromino> nextTetrominos;
+	private Queue<Tetromino> nextTetrominos;
 	private String player;
 	private int score;
 	private int lvl;
 
 	public Game() {
 		init();
-		
-		//testing rotation
-		printMatrix(nextTetrominos.get(0));
-		printMatrix(rotateTetromino(nextTetrominos.get(0)));
+
+		// testing rotation
+		Tetromino testTetro = nextTetrominos.element();
+		for (int i = 0; i < 4; i++) {
+			printMatrix(testTetro);
+			testTetro.setMatrix(rotateTetromino(testTetro).getMatrix());
+		}
 	}
 
 	private void init() {
 		tetrominos = new ArrayList<>();
-		nextTetrominos = new Stack<>();
+		nextTetrominos = new ArrayBlockingQueue<Tetromino>(LIMIT);
 
 		tetrominos.add(new I());
 		tetrominos.add(new O());
@@ -34,7 +38,7 @@ public class Game {
 		tetrominos.add(new T());
 
 		for (int i = 0; i < LIMIT; i++) {
-			nextTetrominos.push(randomTetromino());
+			nextTetrominos.add(randomTetromino());
 		}
 
 	}
@@ -44,31 +48,31 @@ public class Game {
 		int tetro = (int) (Math.random() * tetrominos.size());
 		return tetrominos.get(tetro);
 	}
-	
+
 	private Tetromino rotateTetromino(Tetromino tetromino) {
 		int size = 4;
 		int[][] rotated = new int[size][size];
 
-	    for (int i = 0; i < size; ++i) {
-	        for (int j = 0; j < size; ++j) {
-	            rotated[i][j] = tetromino.getMatrix()[size - j - 1][i];
-	        }
-	    }
-	    
-	    tetromino.setMatrix(rotated);
+		for (int i = 0; i < size; ++i) {
+			for (int j = 0; j < size; ++j) {
+				rotated[i][j] = tetromino.getMatrix()[size - j - 1][i];
+			}
+		}
+
+		tetromino.setMatrix(rotated);
 		return tetromino;
 	}
-	
+
 	private void printMatrix(Tetromino tetromino) {
-		
+
 		int[][] matrix = tetromino.getMatrix();
-		
-		for(int[] i : matrix) {
-			for(int j : i) {
+
+		for (int[] i : matrix) {
+			for (int j : i) {
 				System.out.print(" " + j + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();		
+		System.out.println();
 	}
 }
--- src/game/Tetromino.java
+++ src/game/Tetromino.java
@@ -1,7 +1,5 @@
 package game;
 
-//import java.util.List;
-
 public interface Tetromino {
 	int[][] getMatrix();
 	void setMatrix(int[][] matrix);
