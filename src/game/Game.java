0b25bcabe8360742e0be4c36df608fa032206fe1
Mario Schuetz <mayo2@gmx.de>
2018-04-17 13:34:09 +0200

    Restructure movement down, detect game over

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,27 +5,34 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
+import test.Test;
+
 public class Game {
-	
+
+	Test test = new Test();
+
 	private List<Tetromino> tetrominos;
 	private Queue<Tetromino> nextTetrominos;
-	private int[] currTetrominoCoords;
+	private int[] currTetrominoCoords; // row + column
 	private final int LIMIT = 2; // current + next
 	private Playfield field;
 	private String player;
 	private int score;
 	private int lvl;
+	private boolean gameOver;
 
 	public Game() {
 		init();
-		tetrominoToField(nextTetrominos.element().getMatrix());
+		pseudoGame();
 	}
 
 	private void init() {
+		gameOver = false;
 		tetrominos = new ArrayList<>();
 		nextTetrominos = new ArrayBlockingQueue<Tetromino>(LIMIT);
 		field = new Playfield();
 		currTetrominoCoords = new int[2];
+		resetCoords();
 		setScore(0);
 		setLvl(1);
 
@@ -43,54 +50,81 @@ public class Game {
 	}
 
 	private Tetromino randomTetromino() {
-
 		int tetro = (int) (Math.random() * tetrominos.size());
 		return tetrominos.get(tetro);
 	}
 
-	private void tetrominoToField(int[][] currTetromino) {
-
-		for (int row = 0; row < currTetromino.length; row++) {
-			for (int column = 0; column < currTetromino.length; column++) {
-
-				field.getMatrix()[row][column + 3] = currTetromino[row][column];
-			}
+	private void tetrominoToField(boolean next) {
+		if (next) {
+			gameOver = gameOver();
+			System.out.println("next tetromino");
+			nextTetrominos.remove();
+			nextTetrominos.add(randomTetromino());
+			test.printMatrix(nextTetrominos.element().getMatrix());
+			resetCoords();
 		}
-		setCurrTetrominoCoords(new int[] { 0, 3 });
+		if (!gameOver) {
+			int[][] currTetromino = nextTetrominos.element().getMatrix();
+			int[][] field = this.field.getMatrix();
+			for (int row = 0; row < currTetromino.length; row++) {
+				for (int column = 0; column < currTetromino.length; column++) {
+						if (field[0][column + 3] == 0 && currTetromino[row][column] == 1)
+							field[row][column + 3] = currTetromino[row][column];
+				}
+			}
+
+		} else
+			System.out.println("GAME OVER");
+	}
+	
+	private boolean gameOver() {
+		if(currTetrominoCoords[0] == 0 && currTetrominoCoords[1] == 3)
+			return true;
+		
+		return false;
 	}
 
-	private Tetromino rotateTetromino(Tetromino tetromino) {
-		int size = tetromino.getMatrix().length;
+	private int[][] rotateTetromino() {
+		int[][] tetromino = nextTetrominos.element().getMatrix();
+		int size = tetromino.length;
 		int[][] rotated = new int[size][size];
 
 		for (int i = 0; i < size; ++i) {
 			for (int j = 0; j < size; ++j) {
-				rotated[i][j] = tetromino.getMatrix()[size - j - 1][i];
+				rotated[i][j] = tetromino[size - j - 1][i];
 			}
 		}
-
-		tetromino.setMatrix(rotated);
-
-		return tetromino;
+		return rotated;
 	}
 
 	public void moveDown() {
-		int[][] newMatrix = this.field.getMatrix();
+		int[][] newMatrix = field.getMatrix().clone();
+		int[][] currTetromino = nextTetrominos.element().getMatrix();
 		int row = currTetrominoCoords[0];
 		int column = currTetrominoCoords[1];
+		boolean failed = false;
 
 		if (!collisionDown()) {
 
 			for (int r = 3; r >= 0; r--) {
 				for (int c = 0; c < 4; c++) {
-					newMatrix[row + r + 1][column + c] = newMatrix[row + r][column + c];
-					newMatrix[row + r][column + c] = 0;
+					if (newMatrix[row + r + 1][column + c] == 0) {
+						newMatrix[row + r + 1][column + c] = currTetromino[r][c];
+						newMatrix[row + r][column + c] = 0;
+					} else
+						failed = true;
 				}
 			}
-			currTetrominoCoords[0] = row + 1;
-		}
+		} else
+			failed = true;
 
-		this.field.setMatrix(newMatrix);
+		if (failed) {
+			System.out.println("collision down");
+			tetrominoToField(true);
+		} else {
+			currTetrominoCoords[0] = row + 1;
+			field.setMatrix(newMatrix);
+		}
 	}
 
 	private void moveLeft() {
@@ -121,6 +155,7 @@ public class Game {
 
 			for (int c = 3; c >= 0; c--) {
 				for (int r = 0; r < 4; r++) {
+
 					newMatrix[row + r][column + c + 1] = newMatrix[row + r][column + c];
 					newMatrix[row + r][column + c] = 0;
 				}
@@ -134,19 +169,20 @@ public class Game {
 	private boolean collisionDown() {
 		boolean collision = false;
 		int row = currTetrominoCoords[0] + 3;
-		System.out.println("height: " + field.getHEIGHT() + " bottom row: " + row);
-		
-		if(row >= field.getHEIGHT() - 1) return true;
-		
+		if (row >= field.getHEIGHT() - 1)
+			return true;
+
+		// if (newMatrix[row + r + 1][column + c] == 0 && newMatrix[row + r][column + c]
+		// == 1) {
+
 		return collision;
 	}
 
 	private boolean collisionLeft() {
 		boolean collision = false;
 		int column = currTetrominoCoords[1];
-		System.out.println("width: " + field.getWIDTH() + " curr right column: " + column);
-		
-		if(column <= 0  ) return true;
+		if (column <= 0)
+			return true;
 
 		return collision;
 	}
@@ -154,10 +190,39 @@ public class Game {
 	private boolean collisionRight() {
 		boolean collision = false;
 		int column = currTetrominoCoords[1] + 3;
-		System.out.println("width: " + field.getWIDTH() + " curr right column: " + column);
-		if(column >= field.getWIDTH() - 1) return true;
+		if (column >= field.getWIDTH() - 1)
+			return true;
 
 		return collision;
+	}
+	
+	private void resetCoords() {
+		currTetrominoCoords[0] = 0;
+		currTetrominoCoords[1] = 3;
+	}
+
+	// private int[][] getCurrentTetromino() {
+	// int row = currTetrominoCoords[0];
+	// int column = currTetrominoCoords[1];
+	// int[][] field = this.field.getMatrix();
+	// int[][] matrix = new int[4][4];
+	//
+	// for (int r = 0; r <= 3; r++) {
+	// for (int c = 0; c <= 3; c++) {
+	// matrix[r][c] = field[row + r][column + c];
+	// }
+	// }
+	// return matrix;
+	// }
+
+	private void pseudoGame() {
+
+		while (!gameOver) {
+			// for(int i = 0; i < 40; i++) {
+			moveDown();
+			test.printMatrix(field.getMatrix());
+		}
+
 	}
 
 	public String getPlayer() {
