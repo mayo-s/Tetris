36122c78f5f40486e5d8ce286e7642182fa5dbdb
Mario Schuetz <mayo2@gmx.de>
2018-04-27 19:08:46 +0200

    Print Array and ArrayList matrix

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,20 +1,36 @@
 package test;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public Test() {
-		
-	}
-	
-	public void printMatrix(int[][] matrix) {
 
-		for (int[] i : matrix) {
-			for (int j : i) {
-				System.out.print(" " + j + " ");
+	}
+
+	public void printMatrix(Object object, int type) {
+
+		if (type == 1) {
+			int[][] matrix = (int[][]) object;
+			for (int[] i : matrix) {
+				for (int j : i) {
+					System.out.print(" " + j + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}else if (type == 2) {
+			ArrayList<ArrayList<Boolean>> matrix = (ArrayList<ArrayList<Boolean>>) object;
+			for (ArrayList<Boolean> i : matrix) {
+				for (boolean b : i) {
+					int j = 0;
+					if(b) j = 1;
+					System.out.print(" " + j + " ");
+				}
+				System.out.println();
 			}
 			System.out.println();
 		}
-		System.out.println();
 	}
-	
+
 }
