2b77383bdc2f9b578952b9dfae984666d64dded4
Mario Schuetz <mayo2@gmx.de>
2018-05-24 11:05:37 +0200

    Set starting row - ignore empty rows above

--- src/ai/AI.java
+++ src/ai/AI.java
@@ -1,8 +1,50 @@
 package ai;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import elements.Playfield;
+import elements.Tetromino;
+
 public class AI {
 
-	public AI() {
-		
+	Playfield pf;
+	List<Tetromino> tetrominos;
+
+	/**
+	 * @param pf
+	 *            current field
+	 * @param tetrominos
+	 *            list of current and following Tetrominos
+	 */
+	public AI(Playfield pf, List<Tetromino> tetrominos) {
+		this.pf = pf;
+		this.tetrominos = tetrominos;
+
+		evaluate();
 	}
+
+	private void evaluate() {
+		createGraph();
+	}
+
+	// find lowest accessible gap
+	private void createGraph() {
+		int row = emptyRow();
+		int column = 0;
+		ArrayList<ArrayList<Boolean>> matrix = pf.getMatrix();
+
+	}
+
+	@SuppressWarnings("unlikely-arg-type")
+	private int emptyRow() {
+
+		for (ArrayList<Boolean> row : pf.getMatrix()) {
+			if (row.contains(true))
+				return row.indexOf(row);
+		}
+
+		return 0;
+	}
+
 }
