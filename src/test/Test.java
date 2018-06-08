c51239ed56d52604b23cdc42255f69022d7ffdd2
Mario Schuetz <mayo2@gmx.de>
2018-06-08 12:51:46 +0200

    Change matrix print out

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,36 +8,27 @@ public class Test {
 
 	}
 
-	/**
-	 * This will print out your given 2D matrix
-	 * @param object can be int[][] or ArrayList<ArrayList<Boolean>>
-	 * @param type specify your input: 1 for int[][] or for ArrayList<ArrayList<Boolean>>
-	 * 
-	 */
-	public void printMatrix(Object object, int type) {
+	public void printTetromino(int[][] matrix) {
 
-		if (type == 1) {
-			int[][] matrix = (int[][]) object;
-			for (int[] i : matrix) {
-				for (int j : i) {
-					System.out.print(" " + j + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}else if (type == 2) {
-			@SuppressWarnings("unchecked")
-			ArrayList<ArrayList<Boolean>> matrix = (ArrayList<ArrayList<Boolean>>) object;
-			for (ArrayList<Boolean> i : matrix) {
-				for (boolean b : i) {
-					int j = 0;
-					if(b) j = 1;
-					System.out.print(" " + j + " ");
-				}
-				System.out.println();
+		for (int[] i : matrix) {
+			for (int j : i) {
+				System.out.print(" " + j + " ");
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 
+	public void printField(ArrayList<String[]> matrix) {
+		for (String[] i : matrix) {
+			for (String cell : i) {
+				if (cell == null)
+					System.out.print(" 0 ");
+				else
+					System.out.print(" 1 ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
