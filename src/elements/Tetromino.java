0d9b56583726b319d7ff0da5457a9083aab36c1b
Mario Schuetz <mayo2@gmx.de>
2018-07-04 16:40:34 +0200

    Add comment description

--- src/elements/Tetromino.java
+++ src/elements/Tetromino.java
@@ -1,83 +1,89 @@
 package elements;
 
-public class Tetromino{	
+public class Tetromino {
 
 	int[][] tetromino;
 	String color;
 	int row;
 	int column;
-	
-	public Tetromino(String type){
 
-            row = 0;
-            column = 3;
-            
-    		switch (type) {
-    		case "I":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {1, 1, 1, 1},
-    	            {0, 0, 0, 0},
-    	            {0, 0, 0, 0}};
-    	        color = "cyan";
-    			break;
-    		case "J":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 0, 1, 0},
-    	            {0, 0, 1, 0},
-    	            {0, 1, 1, 0}};
-    	        color = "blue";
-    			break;
-    		case "L":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 1, 0, 0},
-    	            {0, 1, 0, 0},
-    	            {0, 1, 1, 0}};
-    	            color = "orange";
-    			break;
-    		case "O":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 1, 1, 0},
-    	            {0, 1, 1, 0},
-    	            {0, 0, 0, 0}};
-    	            color = "yellow";
-    			break;
-    		case "S":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 0, 1, 1},
-    	            {0, 1, 1, 0},
-    	            {0, 0, 0, 0}};
-    	            color = "green";
-    			break;
-    		case "T":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 1, 1, 1},
-    	            {0, 0, 1, 0},
-    	            {0, 0, 0, 0}};
-    	            color = "purple";
-    			break;
-    		case "Z":
-    			tetromino = new int[][]{
-    	            {0, 0, 0, 0},
-    	            {0, 1, 1, 0},
-    	            {0, 0, 1, 1},
-    	            {0, 0, 0, 0}};
-    	            color = "red";
-    			break;
-    		default :
-    			tetromino = new int[][]{
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0}};
-                color = "black";
-    			break;
-    		}
+	/**
+	 * @param shape
+	 *            Available values I, J, L, O, S, T, Z
+	 *            - any other value will result in an empty 4x4 int[][]
+	 */
+	public Tetromino(String shape) {
+
+		if (shape == null) shape = "";
+		row = 0;
+		column = 3;
+
+		switch (shape) {
+		case "I":
+			tetromino = new int[][] {
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 } };
+			color = "cyan";
+			break;
+		case "J":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 1, 1, 0 } };
+			color = "blue";
+			break;
+		case "L":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 } };
+			color = "orange";
+			break;
+		case "O":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 } };
+			color = "yellow";
+			break;
+		case "S":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 1, 1 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 } };
+			color = "green";
+			break;
+		case "T":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 1, 1, 1 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 0, 0 } };
+			color = "purple";
+			break;
+		case "Z":
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 1, 1 }, 
+				{ 0, 0, 0, 0 } };
+			color = "red";
+			break;
+		default:
+			tetromino = new int[][] { 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0 } };
+			color = "white";
+			break;
+		}
 	}
 
 	public int[][] getMatrix() {
@@ -85,7 +91,7 @@ public class Tetromino{
 	}
 
 	public void setMatrix(int[][] matrix) {
-		tetromino = matrix;		
+		tetromino = matrix;
 	}
 
 	public String getColor() {
@@ -93,8 +99,8 @@ public class Tetromino{
 	}
 
 	public void setColor(String color) {
-		this.color = color;		
-	}	
+		this.color = color;
+	}
 
 	public int getRow() {
 		return row;
