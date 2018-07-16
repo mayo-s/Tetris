9c8e3c39cf37a82dce3ffde6e124fcbb13fef581
Mario Schuetz <mayo2@gmx.de>
2018-07-16 15:05:56 +0200

    Remove score

--- src/ai/Node.java
+++ src/ai/Node.java
@@ -1,29 +1,24 @@
 package ai;
 
-public class Node implements Comparable<Node>{
-	private int id, rotation, frow, fcolumn, score;
+public class Node implements Comparable<Node> {
+	private int id, rotation, frow, fcolumn;
 	private Integer prevNode;
-	
+
 	public Node(int id, int rotation, int frow, int fcolumn, Integer prevNode) {
 		this.id = id;
 		this.rotation = rotation;
 		this.frow = frow;
 		this.fcolumn = fcolumn;
 		this.prevNode = prevNode;
-		this.score = score;
 	}
-
-//	@Override
-//	public int compareTo(Node o) {
-//		if(this.score > o.getScore()) return 1;
-//		else if(this.score < o.getScore()) return -1;
-//		return 0;
-//	}
 
 	@Override
 	public int compareTo(Node o) {
-		if(this.id > o.getId()) return 1;
-		else if(this.id < o.getId()) return -1;
+		if (this.id > o.getId())
+			return 1;
+		else if (this.id < o.getId())
+			return -1;
+
 		return 0;
 	}
 
@@ -46,5 +41,4 @@ public class Node implements Comparable<Node>{
 	public Integer getPrevNode() {
 		return prevNode;
 	}
-	
 }
