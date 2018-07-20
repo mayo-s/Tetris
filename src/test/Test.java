4024574da51ef1dbea2a655d56736dccb8842439
Mario Schuetz <mayo2@gmx.de>
2018-07-20 22:19:06 +0200

    Add print list of Tetrominos

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,9 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import game.Tetromino;
 
 public class Test {
 
@@ -13,6 +16,21 @@ public class Test {
 		for (int[] i : matrix) {
 			for (int j : i) {
 				System.out.print(" " + j + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public void printAllTetrominos(List<Tetromino> list) {
+		int dimension = list.get(0).getMatrix().length;
+		for (int r = 0; r < dimension; r++) {
+			for (Tetromino t : list) {
+				int[] row = t.getMatrix()[r];
+				for (int c : row) {
+					System.out.print(" " + c + " ");
+				}
+				System.out.print("    <    ");
 			}
 			System.out.println();
 		}
